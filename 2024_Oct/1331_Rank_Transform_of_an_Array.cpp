4c1938fccee6bec890698fe9be6288549070c585
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-10-01 20:31:47 -0700

    Update 1331_Rank_Transform_of_an_Array.cpp

--- 2024_Oct/1331_Rank_Transform_of_an_Array.cpp
+++ 2024_Oct/1331_Rank_Transform_of_an_Array.cpp
@@ -5,20 +5,25 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) 
     {
-        vector<int> ans;
-        set<int> sorted;
-        unordered_map<int, int> umap;
+        vector<int> ans;                // To assign the rank of each element in the order of appearance of the element in the arr
+        set<int> sorted;                // Sorted set of arr in ascending order
+        unordered_map<int, int> umap;   // To store the ranks of each elements
 
+        // Insert each element into the sorted set
         for (int i : arr)
         {
             sorted.insert(i);
         }
+
+        // Store the rank for each element
         int index = 1;
         for (int i : sorted)
         {
             umap[i] = index;
             index++;
         }
+
+        // Assign the rank and return
         for (int i : arr)
         {
             ans.emplace_back(umap[i]);
