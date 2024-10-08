f5ce09fff8f933a8f370bd3c3afa64428c0a6e8f
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-10-08 09:34:42 -0700

    Update 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp

--- 2024_Oct/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
+++ 2024_Oct/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
@@ -4,17 +4,20 @@ class Solution {
 public:
     int minSwaps(string s) 
     {
-        int n = s.length();
-        int open = 0;
-        int close = 0;
-        int ans = 0;
+        int n = s.length();    // length of s 
+        int open = 0;          // Keep track of opening bracket
+        int close = 0;         // Keep track of closing bracket
+        int ans = 0;           // Number of swaps
 
         for (int i = 0; i < n; i++)
         {
-            if (s[i] == '[') open++;
-            else close++;
+            if (s[i] == '[') open++;    // Increment count of opening bracket 
+            else close++;               // Increment count of closing bracket
 
-            if (close > open) 
+            // If, at any point, number of closing bracket is larger than opening bracket, the brackets are not balanced.
+            // Assume that the bracket is swapped with an opening bracket and count of opening bracket is increased and closing bracket is decreased by one.
+            // Increment count for swaps.
+            if (close > open)            
             {
                 open++;
                 ans++;
