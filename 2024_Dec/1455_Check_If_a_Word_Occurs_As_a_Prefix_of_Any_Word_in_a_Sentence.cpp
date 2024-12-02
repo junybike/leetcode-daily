6312f2e0a95419595e99e9fe705a6325f4ba1372
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-12-02 00:28:00 -0700

    Create 1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp

---
+++ 2024_Dec/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
@@ -0,0 +1,30 @@
+// 2024 Dec 3
+
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) 
+    {
+        string word = "";           // A word to check in 'sentence'
+        int space = 1;              // the 'searchWord' is found in ith (space) word in 'sentence' 
+        int n = sentence.length();  // the length of 'sentence'
+
+        for (int i = 0; i < n; i++)    
+        {   
+            word += sentence[i];    // Add character by character until it reaches a space
+            
+            // If the current character is a space or the end character,
+            // check if the 'searchWord' is located at the beginning of 'word'.
+            // If located at the beginning, return the 'space'.
+            // Else, increment the 'space' and reset the 'word'.
+            if (sentence[i] == ' ' || i == n - 1) 
+            { 
+                if (word.find(searchWord) == 0) return space;
+                space++;
+                word = "";
+            }
+        }
+
+        // No searchWord is found at any beginning of 'word'
+        return -1;
+    }
+};
