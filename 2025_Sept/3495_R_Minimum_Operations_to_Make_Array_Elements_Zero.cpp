375b2bff870b770e66e6c5ef1863ad082570755c
Jason Lee <79300253+junybike@users.noreply.github.com>
2025-09-05 20:44:51 -0700

    Create 3495_R_Minimum_Operations_to_Make_Array_Elements_Zero.cpp

---
+++ 2025_Sept/3495_R_Minimum_Operations_to_Make_Array_Elements_Zero.cpp
@@ -0,0 +1,34 @@
+// 2025 Sept 6
+class Solution {
+public:
+
+    long long get(int num)
+    {
+        int i = 1;
+        int base = 1;
+        int cnt = 0;
+
+        while (base <= num)
+        {
+            // to determine the number of operations, need to know the binary number of length  
+            cnt += 1 * (i + 1) / 2 * (min(base * 2 - 1, num) - base + 1);
+            i++;
+            base *= 2;
+        }
+        return cnt;
+    }
+
+    long long minOperations(vector<vector<int>>& queries) 
+    {
+        long long ans = 0;
+        for (auto& q : queries)
+        {
+            ans += (get(q[1]) - get(q[0] - 1) + 1) / 2;
+        }    
+        return ans;
+    }
+};
+/*
+solution reference
+https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/editorial
+*/
