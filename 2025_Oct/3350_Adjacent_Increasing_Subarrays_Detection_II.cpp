41e7c301421680ad4c71b3d9561e165618effc3f
Jason Lee <79300253+junybike@users.noreply.github.com>
2025-10-14 20:01:06 -0700

    Create 3350_Adjacent_Increasing_Subarrays_Detection_II.cpp

---
+++ 2025_Oct/3350_Adjacent_Increasing_Subarrays_Detection_II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int L = 0;
+
+        vector<int> sub(n, 1);              // stores length of increasing sequence ending at i
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i - 1] < nums[i])
+            {
+                sub[i] = sub[i - 1] + 1;
+            }
+        }  
+
+        vector<int> subStart(n, 1);         // stores length of increasing sequence starting from i
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                subStart[i] = subStart[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++)     // find the max length of increasing sequences adjacent to others
+        {   
+            int cur = min(sub[i], subStart[i + 1]); 
+            ans = max(ans, cur);
+        }
+
+        return ans;    
+    }
+};
